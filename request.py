5c00d58f5325b3af1135333152730c2f862074ee
Zarino Zappia <mail@zarino.co.uk>
2013-07-05 15:10:03 +0100

    better unicode support; show date submitted in database

--- request.py
+++ request.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # Takes 5 command line arguments: a name, phone number, an email address, a project description, and an ip address.
 # Saves the data request to a SQLite file.
@@ -6,34 +7,53 @@
 
 import sys
 import sqlite3
+import datetime
 
 
 def main():
     args = sys.argv[1:]
+    log(args)
     
     if len(args) == 5:
         conn = sqlite3.connect('scraperwiki.sqlite')
         cursor = conn.cursor()
         command = """CREATE TABLE IF NOT EXISTS "request" (
-                       "id" INTEGER PRIMARY KEY, "name", "phone", "email", "ip", "description"
-                       "created" DATE DEFAULT (datetime('now', 'localtime'))
+                       "id" INTEGER PRIMARY KEY, "date", "name", "phone", "email", "ip", "description"
                      );"""
         cursor.execute(command)
         command = 'SELECT MAX("id") FROM "request";'
         cursor.execute(command)
 
         id = cursor.fetchone()[0] or 1999
+        name = args[0].decode('utf-8')
+        phone = args[1].decode('utf-8')
+        email = args[2].decode('utf-8')
+        description = args[3].decode('utf-8')
+        ip = args[4].decode('utf-8')
+        date = now()
 
-        command = 'INSERT INTO "request" ("id", "name", "phone", "email", "ip", "description") VALUES (?, ?, ?, ?, ?, ?)'
-        cursor.execute(command, [ id + 1, args[0], args[1], args[2], args[4], args[3] ])
+        command = 'INSERT INTO "request" ("id", "date", "name", "phone", "email", "ip", "description") VALUES (?, ?, ?, ?, ?, ?, ?)'
+        cursor.execute(command, [ id + 1, date, name, phone, email, ip, description ])
         conn.commit()
         conn.close()
-        
+
         print id + 1
 
     else:
-        print 'Please supply 4 arguments: a name, a phone number, an email address and a project description'
+        print 'Please supply 5 arguments: a name, a phone number, an email address, a project description and an IP address'
         sys.exit()
+
+
+def log(thing):
+    if(type(thing) == list):
+        thing = "[%s]" % ', '.join(map(str, thing))
+    with open('log.txt', 'a') as f:
+        f.write("%s %s\n" % (now(), thing))
+
+
+def now():
+    return datetime.datetime.now().replace(microsecond=0).isoformat()
+
 
 if __name__ == "__main__":
     main()
